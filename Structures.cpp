110f6804744e9498df81e8c86b4fea6992470651
DarkMelonMan <77286390+DarkMelonMan@users.noreply.github.com>
2024-10-04 18:02:48 +0700

    Update Structures.cpp
    Закончена работа над структурами. Тесты ещё не сделаны

--- Structures.cpp
+++ Structures.cpp
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <string.h>
+#include <malloc.h>
 #define _CRT_SECURE_NO_WARNINGS
+
+enum EntityTypes {
+	living = 1,
+	monster = 2,
+	player = 3
+};
 
 struct LivingEntity {
 	double healthPoints;
@@ -11,14 +18,15 @@ struct MonsterEntity {
 	double damage;
 };
 struct Item {
-	char name[30];
+	char *name;
 };
 struct WeaponItem {
-	Item weapon;
+	Item item;
 	double damage;
 };
 struct Inventory {
-	Item items[20];
+	Item *items;
+	int count;
 };
 struct PlayerEntity {
 	LivingEntity entity;
@@ -52,16 +60,16 @@ LivingEntity livingEntityInput() {
 	return createInstance(healthPoints, movementSpeed);
 }
 
-void livingEntityOutput(LivingEntity livingEntity, int type) {
+void livingEntityOutput(LivingEntity livingEntity, EntityTypes type) {
 	char entityType[20];
 	switch (type) {
-	case 1:
+	case (int)living:
 		strcpy(entityType, "Living entity");
 		break;
-	case 2:
+	case (int)monster:
 		strcpy(entityType, "Monster entity");
 		break;
-	case 3:
+	case (int)player:
 		strcpy(entityType, "Player entity");
 		break;
 	}
@@ -105,9 +113,107 @@ void monsterEntityAttack(MonsterEntity monster, PlayerEntity player) {
 
 void monsterEntityOutput(MonsterEntity monsterEntity) {
 	printf("Monster entity damage = %lf\n", monsterEntity.damage);
-	livingEntityOutput(monsterEntity.entity, 2);
+	livingEntityOutput(monsterEntity.entity, monster);
+}
+
+Item createInstance(char* name) {
+	Item item;
+	item.name = name;
+	return item;
+}
+
+Item itemInput() {
+	char* name = (char*)calloc(30, sizeof(char));
+	printf("Item name input: ");
+	do {
+		fgets(name, 30, stdin);
+		if (strlen(name) == 0)
+			printf("\nTry again: ");
+	} while (strlen(name) == 0);
+	return createInstance(name);
+}
+
+void itemOutput(Item item) {
+	printf("Item name: %s\n", item.name);
+}
+
+WeaponItem createInstance(char* name, double damage) {
+	WeaponItem weapon;
+	weapon.item = createInstance(name);
+	weapon.damage = damage;
+	return weapon;
+}
+
+WeaponItem weaponInput() {
+	Item weapon = itemInput();
+	double damage;
+	printf("Damage of weapon input: ");
+	do {
+		scanf("%lf", &damage);
+		if (damage <= 0)
+			printf("\nDamage can't be <= 0. Try again: ");
+	} while (damage <= 0);
+	return createInstance(weapon.name, damage);
+}
+
+void weaponItemOutput(WeaponItem weapon) {
+	itemOutput(weapon.item);
+	printf("\nDamage of weapon = %lf", weapon.damage);
+}
+
+Inventory createInstance(Item *items, int count) {
+	Inventory inventory;
+	inventory.items = items;
+	inventory.count = count;
+	return inventory;
+}
+
+Inventory inventoryInput() {
+	int count;
+	printf("Count of inventory items input: ");
+	do {
+		scanf("%d", &count);
+		if (count <= 0 || count > 20)
+			printf("\nCount of inventory items must be between 1 and 20");
+	} while (count <= 0 || count > 20);
+	Item* items = (Item*) calloc(count, sizeof(Item));
+	printf("Names of %d items input: ", count);
+	for (int i = 0; i < count; i++)
+		items[i] = itemInput();
+	return createInstance(items, count);
+}
+
+void inventoryOutput(Inventory inventory) {
+	for (int i = 0; i < inventory.count; i++)
+		itemOutput(inventory.items[i]);
+}
+
+PlayerEntity createInstance(double healthPoints, double movementSpeed, Inventory playerInventory, WeaponItem equippedWeapon) {
+	PlayerEntity player;
+	player.entity = createInstance(healthPoints, movementSpeed);
+	player.playerInventory = playerInventory;
+	player.equippedWeapon = equippedWeapon;
+	return player;
+}
+
+PlayerEntity playerEntityInput() {
+	LivingEntity player = livingEntityInput();
+	Inventory playerInventory = inventoryInput();
+	WeaponItem equippedWeapon = weaponInput();
+}
+
+void playerEntityOutput(PlayerEntity entity) {
+	printf("Player inventory:\n");
+	inventoryOutput(entity.playerInventory);
+	printf("Player weapon:\n");
+	weaponItemOutput(entity.equippedWeapon);
+	livingEntityOutput(entity.entity, player);
 }
 
 void playerEntityHurt(PlayerEntity player, double damage) {
 	livingEntityHurt(player.entity, damage);
+}
+
+void playerEntityAttack(PlayerEntity player, MonsterEntity monster) {
+	monsterEntityHurt(monster, player.equippedWeapon.damage);
 }
